31fd9269bbf121f6996bc684d139e2ffcfab9933
Nguyen Tri Duc <145568749+triduccc@users.noreply.github.com>
2025-12-22 15:37:56 +0700

    added room

---
+++ Game/EscapeRoom/Room.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Room 
+{
+    private ArrayList<GameComponent> contents;
+    private ArrayList<Room> connectedRooms;
+    private boolean isExit;
+    
+    public void exploreRecursive(int depth)
+    {
+        exploreRecursive(depth);
+        for(int i = 0; i < connectedRooms.size(); i++)
+        {
+            exploreRecursive(depth);
+        }
+    }
+
+    public boolean containsItemRecursive(String itemName)
+    {
+        
+    }
+
+    public int maxDepthRecursive()
+    {
+        int maxRoomDepth = 0;
+        for(int i = 0; i < connectedRooms.size(); i++)
+        {
+            int connectedRoomDepth = 
+        }
+    }
+}
