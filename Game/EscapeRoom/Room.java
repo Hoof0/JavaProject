db09a448d6d917e6ed726f404c8a0dca703dc66c
Nguyen Tri Duc <145568749+triduccc@users.noreply.github.com>
2025-12-22 20:31:26 +0700

    Complete the required methos in Room

--- Game/EscapeRoom/Room.java
+++ Game/EscapeRoom/Room.java
@@ -5,19 +5,75 @@ public class Room
     private ArrayList<GameComponent> contents;
     private ArrayList<Room> connectedRooms;
     private boolean isExit;
+    private String roomName;
+
+    public Room()
+    {
+        roomName = "";
+        isExit = false;
+    }
+
+    public Room(String name, boolean exit)
+    {
+        roomName = name;
+        isExit = exit;
+    }
+
+    public boolean getExit()
+    {
+        return isExit;
+    }
+
+    public String getRoomName()
+    {
+        return roomName;
+    }
+
+    public void setExit(boolean exit)
+    {
+        isExit = exit;
+    }
+
+    public void setRoomName(String name)
+    {
+        roomName = name;
+    }
     
     public void exploreRecursive(int depth)
     {
-        exploreRecursive(depth);
+        if(depth == 0)
+        {
+            System.out.println("Room info: " + this.toString()); // Example: print room details
+            return;
+        }
+        if(connectedRooms.isEmpty())
+        {
+            System.out.println("Room info:" + this.toString());
+        }
         for(int i = 0; i < connectedRooms.size(); i++)
         {
-            exploreRecursive(depth);
+            connectedRooms.get(i).exploreRecursive(depth - 1);
         }
     }
 
-    public boolean containsItemRecursive(String itemName)
+    public boolean containsItemRecursive(String itemName) // check if the room and its subroom contain an item
     {
-        
+        for(int i = 0; i < contents.size(); i++)
+        {
+            if(contents.get(i).getName().equals(itemName))
+            {
+                return true;
+            }
+        }
+
+        for(int i = 0; i < connectedRooms.size(); i++)
+        {
+            if(connectedRooms.get(i).containsItemRecursive(itemName))
+            {
+                return true;
+            }
+        }
+        return false;        
     }
 
     public int maxDepthRecursive()
@@ -25,10 +81,43 @@ public class Room
         int maxRoomDepth = 0;
         for(int i = 0; i < connectedRooms.size(); i++)
         {
-            int connectedRoomDepth = maxDepthRecursive();
-            int maxConnectedDepth = Math.max(maxRoomDepth, connectedRoomDepth);
+            maxRoomDepth = Math.max(maxRoomDepth, connectedRooms.get(i).maxDepthRecursive());
+        }
+        return maxRoomDepth + 1;
+    }
+
+    public String toString()
+    {
+        String string = "";
+        string += "Contents:\n";
+        for (int i = 0; i < contents.size(); i++) 
+        {
+            string += contents.get(i).getName();
+            if(i < contents.size() - 1) // the last one will not have ","
+            {
+                string += ", ";
+            }
         }
 
-        return maxConnectedDepth + 1;
+        string += "\nConnected Rooms:";
+        for (int i = 0; i < connectedRooms.size(); i++) 
+        {
+            string += connectedRooms.get(i).getRoomName();
+            if(i < connectedRooms.size() - 1) 
+            {
+                string += ", ";
+            }
+        }
+
+        if(isExit)
+        {
+            string += "\n The room is an exit";
+        }
+        else
+        {
+            string += "\nRoom is not the exit";
+        }
+
+        return string;
     }
 }
