1ae1899e30b7cd032d7826064bbf6601b8df02c3
MrOnion123 <doquanghuy031105@gmail.com>
2025-12-22 15:19:35 +0700

    Create GameComponent.java

---
+++ Game/EscapeRoom/GameComponent.java
@@ -0,0 +1,11 @@
+package Game.EscapeRoom;
+
+public abstract class GameComponent {
+    protected String name;
+
+    public abstract void inspect();
+    public String getName()
+    {
+        return name;
+    }
+}
