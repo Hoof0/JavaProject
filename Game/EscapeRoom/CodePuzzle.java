5f8e829986606857e606734c5e7dbc3e07f102e7
Hoof0 <hho2k5@gmail.com>
2025-12-22 15:52:57 +0700

    Update

---
+++ Game/EscapeRoom/CodePuzzle.java
@@ -0,0 +1,8 @@
+package Game.EscapeRoom;
+
+public class CodePuzzle {
+    
+    public boolean attemptSolve(String answer) throws InvalidPuzzleException {
+        
+    }
+}
---
+++ Game/EscapeRoom/RiddlePuzzle.java
@@ -0,0 +1,10 @@
+package Game.EscapeRoom;
+
+public class RiddlePuzzle extends Puzzle{
+    
+    public boolean attemptSolve(String answer) throws InvalidPuzzleException{
+
+    }
+
+    
+}
