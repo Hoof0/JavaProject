1204ad2762f109032e865841a2ee37ca9ac1c8e7
Hoof0 <hho2k5@gmail.com>
2025-12-23 09:36:35 +0700

    Engine

--- Game/EscapeRoom/GameEngine.java
+++ Game/EscapeRoom/GameEngine.java
@@ -1,7 +1,12 @@
 package Game.EscapeRoom;
 
+import java.util.ArrayList;
+import java.util.Queue;
+
 public class GameEngine {
     private boolean Win = false;
+    private ArrayList<Room> map;
+    Queue<String> hint; 
 
     void start(){
         while (Win != true){
