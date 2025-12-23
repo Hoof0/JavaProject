bdc758a37d2bd3908992f3c3fafaf6ed2c5750af
MrOnion123 <doquanghuy031105@gmail.com>
2025-12-23 15:55:27 +0700

    Update RiddlePuzzle.java

--- Game/EscapeRoom/RiddlePuzzle.java
+++ Game/EscapeRoom/RiddlePuzzle.java
@@ -1,5 +1,9 @@
 
 public class RiddlePuzzle extends Puzzle{
+    public RiddlePuzzle(String name, int difficulty, boolean solved)
+    {
+        super(name, difficulty, solved);
+    }
     
     public boolean attemptSolve(String answer) throws InvalidPuzzleException{
 
