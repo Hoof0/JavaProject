dbe024222f2613ef9c8624ed03bbe0cda920e97b
Nguyen Tri Duc <145568749+triduccc@users.noreply.github.com>
2025-12-23 19:46:51 +0700

    Added pickup command

--- Game/EscapeRoom/GameEngine.java
+++ Game/EscapeRoom/GameEngine.java
@@ -17,6 +17,7 @@ public class GameEngine {
     }
 
     void processCommand(String cmd){
+        Scanner scanner = new Scanner(System.in);
         cmd = cmd.toLowerCase();
         Room curr = player.getCurrentRoom();
         switch (cmd) {
@@ -28,7 +29,6 @@ public class GameEngine {
                 for(int i = 0; i < connectRoom.size(); i++){
                     System.out.println(connectRoom.get(i).toString());
                 }
-                Scanner scanner = new Scanner(System.in);
                 System.out.println("Where would you like to move to");
                 String destination = scanner.nextLine();
                 destination = destination.toLowerCase();
@@ -43,7 +43,39 @@ public class GameEngine {
                 player.goBack();
                 break;
             case "pickup":
-                
+                ArrayList<GameComponent> roomContents = curr.getContents();
+                if(roomContents.isEmpty())
+                {
+                    System.out.println("No items in this room.");
+                }
+                else
+                {
+                    System.out.println("Items to pick up: ");
+                    for(int i = 0; i < roomContents.size(); i++)
+                    {
+                        System.out.println(i + "." + roomContents.get(i).getName());
+                    }
+                    System.out.print("Enter the name of the item you want to pick up: ");
+                    String itemName = scanner.nextLine().trim().toLowerCase();
+                    boolean found = false;
+
+                    for(int i = 0; i < roomContents.size(); i++)
+                    {
+                        if(roomContents.get(i).getName().toLowerCase().equals(itemName))
+                        {
+                            player.pickupItem((Item)roomContents.get(i));
+                        }
+                        roomContents.remove(i);
+                        System.out.println("Picked up successfully.");
+                        found = true;
+                        break;
+                    }
+
+                    if(!found)
+                    {
+                        System.out.println("Item not found in this room.");
+                    }
+                }
                 break;
             default:
                 break;
