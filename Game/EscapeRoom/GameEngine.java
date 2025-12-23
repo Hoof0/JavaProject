c5cb28b63a8ac75602676759f338e67f5acacede
Hoof0 <hho2k5@gmail.com>
2025-12-23 14:43:12 +0700

    Update GameEngine.java

--- Game/EscapeRoom/GameEngine.java
+++ Game/EscapeRoom/GameEngine.java
@@ -2,10 +2,12 @@
 
 import java.util.ArrayList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class GameEngine {
     private boolean Win = false;
     private ArrayList<Room> map;
+    private Player player;
     Queue<String> hint; 
 
     void start(){
@@ -15,15 +17,33 @@ public class GameEngine {
     }
 
     void processCommand(String cmd){
+        cmd = cmd.toLowerCase();
         switch (cmd) {
             case "look":
-                
+                Room curr = player.getCurrentRoom();
+                curr.toString();
                 break;
             case "move":
-                String destination = "";
-                for(int i = 0; i < .getConnectedRoom().size(); i++){
-
+                Room curr = player.getCurrentRoom();
+                ArrayList<Room> connectRoom = curr.getConnectedRoom();
+                for(int i = 0; i < connectRoom.size(); i++){
+                    System.out.println(connectRoom.get(i).toString());
                 }
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Where would you like to move to");
+                String destination = scanner.nextLine();
+                destination = destination.toLowerCase();
+                for(int i = 0; i < connectRoom.size(); i++){
+                    if (destination == connectRoom.get(i).getRoomName().toLowerCase()){
+                        player.moveTo(connectRoom.get(i));
+                    }
+                }
+                scanner.close();
+                break;
+            case "back":
+                break;
+            case "pickup":
+                break;
             default:
                 break;
         }
